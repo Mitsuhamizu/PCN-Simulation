1fb52de595d666c287efedadb385e169635bd82e
ZhiChun <unlock_1@163.com>
2021-03-18 15:39:24 +0800

    read params

--- PCN-Simulation/src/main.rs
+++ PCN-Simulation/src/main.rs
@@ -1,3 +1,24 @@
+use relative_path::RelativePath;
+use serde::Deserialize;
+use std::env;
+use std::path::Path;
+use std::{collections::HashMap, error::Error};
+use std::{fs::File, u32};
+use std::{io::BufReader, path};
+
 fn main() {
-    println!("Hello, world!");
+    // read params
+    let params_path = format!("{}/src/data/params.json", env!("CARGO_MANIFEST_DIR"));
+    let params = match read_json_from_file(&&params_path) {
+        Ok(params) => params,
+        Err(error) => panic!("{:?}", error),
+    };
+    // read csv
+}
+
+fn read_json_from_file(file_path: &String) -> Result<HashMap<String, u32>, Box<dyn Error>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let u = serde_json::from_reader(reader)?;
+    Ok(u)
 }
